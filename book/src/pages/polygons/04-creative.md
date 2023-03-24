7515355374888373f69bceb338ab002c6c321423
Noel Welsh <noel@noelwelsh.com>
2023-03-24 15:53:16 +0000

    Add examples of creative applications

--- book/src/pages/polygons/04-creative.md
+++ book/src/pages/polygons/04-creative.md
@@ -3,3 +3,20 @@
 We're now going to look at a few creative extensions of the basic ideas we've seen so far.
 
 ## Concentric Polygons
+
+@:doodle("concentric", "Creative.concentricExercise")
+
+
+## Polygon spiral
+
+@:doodle("spiral", "Creative.spiralExercise")
+
+
+## Curvygons
+
+@:doodle("curvygon", "Creative.curvygonExercise")
+
+
+## Rosettes
+
+@:doodle("rosette", "Creative.rosetteExercise")
--- examples/src/main/scala/polygons/Creative.scala
+++ examples/src/main/scala/polygons/Creative.scala
@@ -27,20 +27,134 @@ object Creative {
 
   @JSExport
   def concentricExercise(id: String): Unit = {
+    def style(percentage: Double, image: Image): Image = {
+      image
+        .strokeColor(
+          Color.crimson.darkenBy((percentage * percentage).normalized)
+        )
+        .strokeWidth(3.0)
+    }
+
     def concentricPolygons(
-        count: Int,
+        total: Int,
         startRadius: Double,
         radiusStep: Double
-    ): Image =
-      count match {
-        case 0 =>
-          regularPolygon(3, startRadius)
-            .strokeColor(Color.crimson)
-            .strokeWidth(3.0)
-        case n =>
-          concentricPolygons(n - 1, startRadius, radiusStep).on(
-            regularPolygon(n, startRadius + (radiusStep * n))
+    ): Image = {
+      def loop(count: Int): Image =
+        count match {
+          case 0 =>
+            regularPolygon(3, startRadius)
+              .strokeColor(Color.crimson)
+              .strokeWidth(3.0)
+          case n =>
+            loop(n - 1).on(
+              style(
+                n.toDouble / total.toDouble,
+                regularPolygon(n + 3, startRadius + (radiusStep * n))
+              )
+            )
+        }
+
+      loop(total)
+    }
+
+    concentricPolygons(7, 20, 10).drawWithFrame(Frame(id))
+  }
+
+  @JSExport
+  def spiralExercise(id: String): Unit = {
+    def style(percentage: Double, image: Image): Image = {
+      image
+        .strokeColor(
+          Color.turquoise.darkenBy((percentage * percentage).normalized)
+        )
+        .strokeWidth(3.0)
+    }
+
+    def polygonSpiral(
+        total: Int,
+        startRadius: Double,
+        radiusStep: Double,
+        angleStep: Angle
+    ): Image = {
+      def loop(count: Int): Image =
+        count match {
+          case 0 => style(0.0, regularPolygon(5, startRadius))
+          case n =>
+            loop(count - 1).on(
+              style(
+                count.toDouble / total,
+                regularPolygon(5, startRadius + (radiusStep * n))
+                  .rotate(angleStep * n)
+              )
+            )
+        }
+
+      loop(total)
+    }
+    polygonSpiral(15, 20, 7, 5.degrees).drawWithFrame(Frame(id))
+  }
+
+  @JSExport
+  def curvygonExercise(id: String): Unit = {
+    def regularCurvygon(
+        sides: Int,
+        radius: Int,
+        radiusOffset: Int,
+        offset1: Double,
+        offset2: Double
+    ): Image = {
+      val turn = (1.0 / sides).turns
+      def loop(count: Int): ClosedPath =
+        count match {
+          case 0 => ClosedPath.empty.moveTo(radius, 0.degrees)
+          case n =>
+            loop(n - 1).curveTo(
+              radius - radiusOffset,
+              turn * (n - 1) + (turn * offset1),
+              radius + radiusOffset,
+              turn * (n - 1) + (turn * offset2),
+              radius,
+              turn * n
+            )
+        }
+
+      Image.path(loop(sides))
+    }
+
+    -70.0
+      .upToIncluding(70.0)
+      .map(offset => regularCurvygon(5, 100, offset.toInt, 0.2, 0.7).compile)
+      .forDuration(2.5.seconds)
+      .andThen(_ =>
+        70.0
+          .upToIncluding(-70.0)
+          .map(offset =>
+            regularCurvygon(5, 100, offset.toInt, 0.2, 0.7).compile
           )
-      }
+          .forDuration(2.5.seconds)
+      )
+      .repeatForever
+      .animate(Frame(id).withSize(250, 250))
+  }
+
+  @JSExport
+  def rosetteExercise(id: String): Unit = {
+    def rosette(sides: Int, radius: Int, skip: Int): Image = {
+      val turn = (1.0 / sides).turns
+      def loop(count: Int): ClosedPath =
+        count match {
+          case 0 => ClosedPath.empty.moveTo(radius, 0.degrees)
+          case n =>
+            loop(n - 1).lineTo(radius, turn * (n * skip))
+        }
+
+      Image.path(loop(sides))
+    }
+
+    rosette(7, 100, 3)
+      .beside(rosette(8, 100, 3))
+      .beside(rosette(10, 100, 3))
+      .drawWithFrame(Frame(id))
   }
 }
