af1d27ad35c047aaf550db085cfdc7918d73a027
Noel Welsh <noel@noelwelsh.com>
2023-05-03 16:01:23 +0100

    New table of contents
    - Collapse all chapters except the currently viewed one
    - Create JS script to do this
    - Update CSS to match changes
    - Add directory.conf for programs chapter

---
+++ book/src/js/toc.js
@@ -0,0 +1,84 @@
+
+function tocPatch() {
+  const node = document.querySelector('ul.nav-list');
+
+  // () => Array[Array[Element]]
+  function makeChapters() {
+    // Array[Array[Element]]
+    //
+    // Each Element is a TOC link. The first element is the chapter title
+    const chapters = [];
+
+    Array.from(node.children).forEach(item => {
+      if (item.classList.contains('level1')) {
+        chapters.push([item]);
+      } else {
+        const chapter = chapters.pop();
+        chapter.push(item);
+        chapters.push(chapter);
+      }
+    })
+
+    return chapters;
+  }
+
+  // Element => Element
+  //
+  // Input should be of the form <li><a href="...">title</a></li>
+  function makeChapterTitle(element) {
+    const header = document.createElement("h5");
+    header.replaceChildren(...element.children);
+
+    return header;
+  }
+
+  // Array[Element] => Boolean
+  function isActive(elements) {
+    return elements.some(elt => elt.classList.contains('active'))
+  }
+
+  // Array[Element] => Element
+  function makeChapterToc(chapter) {
+    const first = chapter.shift();
+    const title = makeChapterTitle(first);
+
+    if (first.classList.contains('nav-leaf')) {
+      const li = document.createElement('li');
+      li.className = first.className;
+      li.appendChild(title);
+      return li;
+    } else {
+      const children = [];
+
+      const summary = document.createElement('summary');
+      summary.appendChild(title);
+      children.push(summary);
+
+      const list = document.createElement('ul');
+      list.replaceChildren(...chapter);
+      children.push(list);
+
+      const details = document.createElement('details');
+      if (isActive(chapter)) {
+        details.setAttribute('open', 'true');
+      }
+      details.replaceChildren(...children);
+
+      const li = document.createElement('li');
+      li.className = first.className;
+      li.appendChild(details);
+      return li;
+    }
+  }
+
+  function makeToc() {
+    const chapters = makeChapters();
+    console.log("chapters", chapters);
+    const toc = chapters.map(chapter => makeChapterToc(chapter));
+
+    node.replaceChildren(...toc);
+  }
+
+  makeToc();
+}
+addEventListener('load', () => tocPatch());
