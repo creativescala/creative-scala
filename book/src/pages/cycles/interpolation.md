317531b0a00c629314a06f1748802c98ae5dbce9
Noel Welsh <noel@noelwelsh.com>
2023-06-07 14:19:06 +0100

    Start on material on interpolation

--- book/src/pages/cycles/interpolation.md
+++ book/src/pages/cycles/interpolation.md
@@ -1,2 +1,27 @@
 # Interpolation
 
+```scala mdoc:invisible
+import doodle.core._
+import doodle.image._
+import doodle.syntax.all._
+import doodle.image.syntax.all._
+import doodle.java2d._
+```
+
+Interpolation is our final topic before we can create the pictures we're after.
+Interpolation means guessing at a value inbetween points.
+In our case we're going to use interpolation to create smooth curves that connect the points we sample from our parametric curves.
+
+The example below sampling between 3 and 15 points from a parametric circle, and then interpolating a curve between these points.
+
+@:doodle("circle-interpolation", "CyclesInterpolation.circleInterpolation")
+
+We can see that the curve more closely resembles a circle as we increase the number of points we base it off.
+
+We use the `Image.interpolatingSpline` method to interpolate a curve. 
+The method expects a `List[Point]`, which is a type that means a list of `Points`. 
+We haven't worked with lists before, so it's time to learn a little bit about them.
+
+A `List` is what's known as a container, meaning it holds other values.
+
+We can construct a 
---
+++ examples/src/main/scala/cycles/Interpolation.scala
@@ -0,0 +1,65 @@
+package cycles
+
+import cats.syntax.all.*
+import doodle.core.*
+import doodle.syntax.all.*
+import doodle.image.*
+import doodle.image.syntax.all.*
+import doodle.interact.animation.*
+import doodle.interact.syntax.all.*
+import doodle.svg.*
+import scala.concurrent.duration.*
+import cats.effect.unsafe.implicits.global
+import scala.scalajs.js.annotation.*
+import doodle.syntax.angle
+import cycles.Epicycles.parametricCircle
+
+@JSExportTopLevel("CyclesInterpolation")
+object Interpolation {
+
+  def sample(samples: Int, curve: Angle => Point): List[Point] = {
+    val step = Angle.one / samples
+    def loop(count: Int): List[Point] = {
+      val angle = step * count
+      count match {
+        case 0 => List.empty
+        case n =>
+          curve(angle) :: loop(n - 1)
+      }
+    }
+
+    loop(samples)
+  }
+
+  val dot = Picture.circle(5).noStroke.fillColor(Color.hotpink)
+
+  def animateInterpolation(
+      minSamples: Int,
+      maxSamples: Int,
+      curve: Angle => Point
+  ): Transducer[Picture[Unit]] = {
+    val I = doodle.interact.animation.Interpolation
+
+    (minSamples
+      .to(maxSamples))
+      .map { s =>
+        val pts = sample(s, curve)
+        val dots = pts.map(pt => dot.at(pt)).allOn
+        // Make the curve end at it's start
+        val c = Picture
+          .interpolatingSpline(pts :+ pts.head)
+          .strokeColor(Color.darkBlue)
+        I.constant(dots.on(c)).forDuration(1.seconds)
+      }
+      .toList
+      .combineAll
+  }
+
+  @JSExport
+  def circleInterpolation(id: String): Unit = {
+    val parametricCircle = (angle: Angle) => Point(100, angle)
+    animateInterpolation(3, 15, parametricCircle).repeatForever.animate(
+      Frame(id).withSize(220, 220)
+    )
+  }
+}
